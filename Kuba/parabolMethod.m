3a3dffd441ad692fd70d85d2d27ae14cf2490451
gogolon <qbidev@protonmail.com>
2022-12-14 15:32:22 +0100

    Add comments

--- Kuba/parabolMethod.m
+++ Kuba/parabolMethod.m
@@ -14,9 +14,12 @@ function [x_0, iter] = parabolMethod(a, x, eps, max_iter)
 steps = 0;
 while(steps < max_iter)
 steps = steps + 1;
+% obliczamy f(x) oraz pierwsza i druga pochodna
 f_x = find_cos(a, x);
 f_x_prim = find_cos_dw(a, x);
 f_x_bis = find_cos_ddw(a, x);
+
+% rozwiazujemy rownanie 0 = f_x + f_x_prim * y + 1/2 f_x_bis * y^2  wzgledem y
 
 w1 = 1/2 * f_x_bis;
 w2 = f_x_prim;
@@ -38,12 +41,16 @@ if isempty(r)
     return;
 end
 
+% wybieramy ten pierwiastek rownania kwadratowego, ktory jest blizszy x
 y = r(1);
 if(abs(r(1) - x) > abs(r(2) - x))
     y = r(2);
 end
+
+% obliczamy kolejne przyblizenie miejsca zerowego
 x = x + y;
 
+% jesli osiagnelismy zadaną dokladnosc, zwracamy wynik
 if(abs(y) < eps)
     x_0 = x;
     iter = steps;
